06b66acad1aa4ad75f65d0a211ebf73a2c29b697
Adam Grace <S00188523@atu.ie>
2023-10-26 23:37:01 +0100

    App complete and working, items in spinners updated to show each color and clear button implemented

--- app/src/main/java/com/example/project_resistor_app/MainActivity.java
+++ app/src/main/java/com/example/project_resistor_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.project_resistor_app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,17 +22,28 @@ public class MainActivity extends AppCompatActivity {
         spinnerColor3 = findViewById(R.id.spinnerColor3);
         tvResult = findViewById(R.id.tvResult);
 
-        String[] bandEntries = {"Band 1", "Band 2", "Band 3"};
+        String[] bandEntries = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White"};
 
-        // Set up adapters for the three spinners
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, bandEntries);
+        // Set up adapters for the three spinners using the custom adapter
+        ArrayAdapter<String> adapter = new ColoredSpinnerAdapter(this, android.R.layout.simple_spinner_item, bandEntries);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerColor1.setAdapter(adapter);
         spinnerColor2.setAdapter(adapter);
         spinnerColor3.setAdapter(adapter);
+
+        // set up the "Clear" button
+        Button btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearValues();
+            }
+        });
     }
 
+
     public void onCalculate(View view) {
+        // Implement the calculation logic here
         int value = calculateResistorValue(
                 spinnerColor1.getSelectedItemPosition(),
                 spinnerColor2.getSelectedItemPosition(),
@@ -53,5 +65,13 @@ public class MainActivity extends AppCompatActivity {
         int resistance = (e12Series[band1] * 10 + e12Series[band2]) * (int) Math.pow(10, band3);
 
         return resistance;
+    }
+
+    private void clearValues() {
+        // Clear the selected values and result
+        spinnerColor1.setSelection(0);
+        spinnerColor2.setSelection(0);
+        spinnerColor3.setSelection(0);
+        tvResult.setText("Result: ");
     }
 }
